c3fd27d27df697ae6a223841937a4de34cf61139
Kunta-pro <smartedupexa@gmail.com>
2024-10-30 14:44:28 +0300

    Add files via upload

--- _footer.php
+++ _footer.php
@@ -1,47 +1,51 @@
-<footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <!-- Insert website logo -->
-          <img src="images/company-logo.jpg" alt="Website Logo">
-          <!-- Display visitor count -->
-          <p>SCHOOL MANAGEMENT SYSTEM</p>
-          <!-- Display time zone -->
-          <p>Q9P3+75H, My Town, My city, My Country </p>
-        </div>
-        <div class="col-md-4">
-          <p>Time Zone: <?php
-                        date_default_timezone_set('Asia/Kolkata');
-                        $current_time = date('D M d Y H:i:s \G\M\TO (T)');
-                        echo "<p>$current_time</p>";
-                        ?></p>
-        </div>
-        <div class="col-md-4">
-          <div class="footer-links">
-           
-          <p>follow us on</p>
-          </div>
-          <div class="social-icons">
-            <a href="#"><i class="fab fa-facebook-f facebook"></i></a>
-            <a href="#"><i class="fa-brands fa-x-twitter twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in linked-in"></i></a>
-          </div>
-        </div>
-      </div>
-      <div class="row mt-4">
-        <div class="col-md-12">
-          <p>&copy; <?php echo date('Y'); ?> By <a href="https://www.github.com/ProjectsAndPrograms" target="_blank">ProjectsAndPrograms</a>. All rights reserved.</p>
-        </div>
-      </div>
-    </div>
-  </footer>
 
 
+    <script src="../assets/js/logout.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js" type="text/javascript"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
+   
 
-  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
-  <script src="js/bootstrap.bundle.js"></script>
-  <script src="./shared/app.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    
+
+    <script src="script.js"></script>
+   
+         
+    <script>
+     
+      function switchToTab(tabId) {
+        if (event) {
+        event.preventDefault();
+      }
+    
+        var tab = new bootstrap.Tab(document.getElementById(tabId));
+        tab.show();
+       
+      }
+    
+      
+     
+      function isTabAvailable(tabId) {
+        return document.getElementById(tabId) !== null;
+      }
+      
+      document.addEventListener('DOMContentLoaded', function () {
+        var hash = window.location.hash;
+        if (hash) {
+          var tabId = hash.substring(1);
+          if (isTabAvailable(tabId)) {
+            switchToTab(tabId);
+          } else {
+            window.location.href = window.location.href.split('#')[0];
+          }
+        }
+      });
+     
+    </script>
+
+   
+   
 </body>
 
 </html>
+
--- _sidebar.php
+++ _sidebar.php
@@ -1,4 +1,3 @@
-
 <div class="sidebar">
     <a href="dashboard.php" class="logo">
         <!-- <i class='bx bx-book-bookmark'></i> -->
@@ -6,24 +5,24 @@
           <div class="logo-name"><span class="text-warning">E</span><span class="darkTextColor">R</span><span class="text-warning">P</span></div>
     </a>
     
-      <ul class="side-menu-opener">
+    <ul class="side-menu-opener">
         <!-- <li>
             <div class='open-arrow SidebarOpener'><i class='bx bxs-chevron-right'></i></div>
         </li> -->
     </ul>
+
     
     <ul class="side-menu main-side-board">
         <li><a href="dashboard.php"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
-        <li><a href="teacher.php"><i class='bx bxs-user-rectangle'></i>Teacher</a></li>
         <li><a href="student.php"><i class='bx bxs-user-detail'></i>Student</a></li>
-        <li><a href="subjects.php"><i class='bx bx-book-bookmark'></i>Subjects</a></li>
+        <!-- <li><a href="subjects.php"><i class='bx bx-book-bookmark'></i>Subjects</a></li> -->
         <li><a href="attendence.php"><i class='bx bx-list-check'></i>Attendence</a></li>
-        <li><a href="noticeboard.php"><i class='bx bx-bookmark'></i>Notice Board</a></li>
+        <li><a href="noticeboard.php"><i class='bx bxs-bookmark'></i>Notice Board</a></li>
         <li><a href="timetable.php"><i class='bx bx-table'></i>Time Table</a></li>
-        <li><a href="syllabus.php"><i class='bx bx-file-blank'></i>Syllabus</a></li>
+        <li><a href="syllabus.php"><i class='bx bxs-file-blank'></i>Syllabus</a></li>
         <li><a href="notes.php"><i class='bx bx-note'></i>Notes</a></li>
         <li><a href="marks.php"><i class='bx bx-paste'></i>Marks</a></li>
-        <li><a href="buses.php"><i class='bx bxs-bus'></i>Bus Service</a></li>
+        <li><a href="leaves.php"><i class='bx bxs-hourglass-top'></i>Leaves</a></li>
         <li><a href="settings.php"><i class='bx bx-cog'></i>Settings</a></li>
     </ul>
     <ul class="side-menu">

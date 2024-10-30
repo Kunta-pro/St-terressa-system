db9ae063f2dfa4ec19061438b7c0a81b8a344d0a
Kunta-pro <smartedupexa@gmail.com>
2024-10-30 14:22:02 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
-    include("../../assets/config.php");
+    $server = "localhost";
+   
+    $user = "root";
+    $password = "";
+    $db = "_sms";
+    
+    $conn = mysqli_connect($server, $user, $password, $db);
+
+    if (!$conn) {
+        header('Location: ../errors/error.html');
+        exit();
+    }
+
 
 ?>
